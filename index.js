30b437da2d832b9c7d710151228851ce94819483
Jiju <178104698+jiju-codes@users.noreply.github.com>
2024-11-04 00:21:14 -0500

    MVP successful

--- index.js
+++ index.js
@@ -1,16 +1,78 @@
-// TODO: Include packages needed for this application using import
+// Dependencies
 import inquirer from 'inquirer';
 import fs from 'fs';
 import generateMarkdown from './utils/generateMarkdown.js';
+import colors from 'colors';
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+	type: 'input',
+	name: 'title',
+	message: colors.magenta('What is the title of your project?'),
+  },
+  {
+	type: 'input',
+	name: 'description',
+	message: colors.magenta('Provide a description of your project:'),
+  },
+  {
+	type: 'input',
+	name: 'installation',
+	message: colors.magenta('What are the installation instructions?'),
+  },
+  {
+	type: 'input',
+	name: 'usage',
+	message: colors.magenta('Provide usage information:'),
+  },
+  {
+	type: 'list',
+	name: 'license',
+	message: colors.magenta('What license is the project under?'),
+	choices: ['MIT', 'GPLv3', 'Apache 2.0', 'None', 'Add manually'],
+  },
+  {
+	type: 'input',
+	name: 'contributing',
+	message: colors.magenta('Provide contribution guidelines:'),
+  },
+  {
+	type: 'input',
+	name: 'tests',
+	message: colors.magenta('Provide test instructions:'),
+  },
+  {
+	type: 'input',
+	name: 'email',
+	message: colors.magenta('What is your email address?'),
+  },
+  {
+	type: 'input',
+	name: 'github',
+	message: colors.magenta('What is your GitHub username?'),
+  },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+	if (err) {
+	  console.error(err);
+	  console.error(colors.red(err));
+	}
+	console.log(colors.green('README.md Successfully created!'));
+  });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to initialize app
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+	const markdown = generateMarkdown(answers);
+	writeToFile('README.md', markdown);
+  }).catch((error) => {
+	console.error(colors.red('Error initializing app:'), error);
+  });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,87 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function that returns a license badge based on which license is passed in
+function renderLicenseBadge(license) {
+  if (license === "MIT") {
+    return "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)";
+  } else if (license === "GPLv3") {
+    return "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)";
+  } else if (license === "Apache 2.0") {
+    return "![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)";
+  } else if (license === "none" || license === "add manually") {
+    return "";
+  } else {
+    return "";
+  }
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Function that returns the license link
+function renderLicenseLink(license) {
+  if (license === "MIT") {
+    return "https://opensource.org/licenses/MIT";
+  } else if (license === "GPLv3") {
+    return "https://www.gnu.org/licenses/gpl-3.0";
+  } else if (license === "Apache 2.0") {
+    return "https://www.apache.org/licenses/LICENSE-2.0";
+  } else if (license === "none" || license === "add manually") {
+    return "";
+  } else {
+    return "";
+  }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Function that returns the license section of README
+function renderLicenseSection(license) {
+  if (license === "MIT") {
+    return "This project is licensed under the MIT license.";
+  } else if (license === "GPLv3") {
+    return "This project is licensed under the GPLv3 license.";
+  } else if (license === "Apache 2.0") {
+    return "This project is licensed under the Apache 2.0 license.";
+  } else if (license === "none") {
+    return "This project does not have a license.";
+  } else if (license === "add manually") {
+    return "Enter license information here.";
+  } else {
+    return "";
+  }
+}
 
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+  
+  ## Description
+${data.description}
 
-`;
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+${renderLicenseSection(data.license)}
+${renderLicenseLink(data.license)}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+If you have any questions, please open an issue or contact me at ${data.email} 
+You can find more of my work at [${data.github}](https://github.com/${
+    data.github
+  }).`;
 }
 
 export default generateMarkdown;

cbcfce2e2a9044eaf65f50213ded65c2829ca5e0
Jiju <178104698+jiju-codes@users.noreply.github.com>
2024-11-04 09:11:17 -0500

    Link to GitHub

--- index.js
+++ index.js
@@ -1,77 +1,80 @@
 // Dependencies
-import inquirer from 'inquirer';
-import fs from 'fs';
-import generateMarkdown from './utils/generateMarkdown.js';
-import colors from 'colors';
+import inquirer from "inquirer";
+import fs from "fs";
+import generateMarkdown from "./utils/generateMarkdown.js";
+import colors from "colors";
 
 const questions = [
   {
-	type: 'input',
-	name: 'title',
-	message: colors.magenta('What is the title of your project?'),
+    type: "input",
+    name: "title",
+    message: colors.magenta("What is the title of your project?"),
   },
   {
-	type: 'input',
-	name: 'description',
-	message: colors.magenta('Provide a description of your project:'),
+    type: "input",
+    name: "description",
+    message: colors.magenta("Provide a description of your project:"),
   },
   {
-	type: 'input',
-	name: 'installation',
-	message: colors.magenta('What are the installation instructions?'),
+    type: "input",
+    name: "installation",
+    message: colors.magenta("What are the installation instructions?"),
   },
   {
-	type: 'input',
-	name: 'usage',
-	message: colors.magenta('Provide usage information:'),
+    type: "input",
+    name: "usage",
+    message: colors.magenta("Provide usage information:"),
   },
   {
-	type: 'list',
-	name: 'license',
-	message: colors.magenta('What license is the project under?'),
-	choices: ['MIT', 'GPLv3', 'Apache 2.0', 'None', 'Add manually'],
+    type: "list",
+    name: "license",
+    message: colors.magenta("What license is the project under?"),
+    choices: ["MIT", "GPLv3", "Apache 2.0", "None", "Add manually"],
   },
   {
-	type: 'input',
-	name: 'contributing',
-	message: colors.magenta('Provide contribution guidelines:'),
+    type: "input",
+    name: "contributing",
+    message: colors.magenta("Provide contribution guidelines:"),
   },
   {
-	type: 'input',
-	name: 'tests',
-	message: colors.magenta('Provide test instructions:'),
+    type: "input",
+    name: "tests",
+    message: colors.magenta("Provide test instructions:"),
   },
   {
-	type: 'input',
-	name: 'email',
-	message: colors.magenta('What is your email address?'),
+    type: "input",
+    name: "email",
+    message: colors.magenta("What is your email address?"),
   },
   {
-	type: 'input',
-	name: 'github',
-	message: colors.magenta('What is your GitHub username?'),
+    type: "input",
+    name: "github",
+    message: colors.magenta("What is your GitHub username?"),
   },
 ];
 
 // Function to write README file
 function writeToFile(fileName, data) {
   fs.writeFile(fileName, data, (err) => {
-	if (err) {
-	  console.error(err);
-	  console.error(colors.red(err));
-	}
-	console.log(colors.green('README.md Successfully created!'));
+    if (err) {
+      console.error(err);
+      console.error(colors.red(err));
+    }
+    console.log(colors.green("README.md Successfully created!"));
   });
 }
 
 // Function to initialize app
 function init() {
-  inquirer.prompt(questions).then((answers) => {
-	const markdown = generateMarkdown(answers);
-	writeToFile('README.md', markdown);
-  }).catch((error) => {
-	console.error(colors.red('Error initializing app:'), error);
-  });
+  inquirer
+    .prompt(questions)
+    .then((answers) => {
+      const markdown = generateMarkdown(answers);
+      writeToFile("README.md", markdown);
+    })
+    .catch((error) => {
+      console.error(colors.red("Error initializing app:"), error);
+    });
 }
 
 // Function call to initialize app
